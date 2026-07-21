355e2ba0f2a3d2d9314d7395495662209fd59c9c
Artlyse.dev <franxrooswvelt@gmail.com>
2026-07-21 16:22:33 -0500

    Add method to find Tecnico by email

--- src/main/java/pe/edu/utp/autofast/repository/TecnicoRepository.java
+++ src/main/java/pe/edu/utp/autofast/repository/TecnicoRepository.java
@@ -1,13 +1,16 @@
 package pe.edu.utp.autofast.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import pe.edu.utp.autofast.entity.Tecnico;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TecnicoRepository extends JpaRepository<Tecnico, Long> {
     List<Tecnico> findByActivoTrueOrderByNombreAsc();
+    
+    // Buscar técnico por email (para sincronización)
+    Optional<Tecnico> findByEmail(String email);
 }
